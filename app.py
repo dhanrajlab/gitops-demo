9d08b14527fdb365273b1cc97cce4f77fe0af2bc
dhanraj <dhanrajn5363@gmail.com>
2023-04-21 07:50:08 +0000

    welcome note changed

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "Hello, every one lets play argocd "
+    return "Hello, Suresh have a great day "
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
