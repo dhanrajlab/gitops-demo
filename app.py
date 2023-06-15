544558afa3692417f46907fd30774db0d804e251
dhanrajlab <125533835+dhanrajlab@users.noreply.github.com>
2023-06-15 16:29:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "Hello everyone have a great day "
+    return "Hello shantha have a great day "
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
